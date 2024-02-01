90876ffdb96cb4c7e04ac14ee1c5adbc5f25855b
johann <johann.cavallucci@epitech.eu>
2024-02-01 23:41:57 +0100

    FEAT - meta

--- app.vue
+++ app.vue
@@ -4,8 +4,9 @@ useHead({
   title: appName,
   meta: [
     { name: "viewport", content: "width=device-width, initial-scale=1" },
-    { name: "keywords", content: "Home" },
-    { name: "description", content: "Nuxt Starter" },
+    { name: "keywords", content: "Portfolio Johann Cavallucci, johanncavallucci, johann cavallucci, JOHANN CAVALLUCCI" },
+    { name: "description", content: "The portfolio of Johann Cavallucci, a web developer." },
+    { name: "author", content: "Johann Cavallucci" },
   ],
   link: [
     {
