7ffd9b6f4e9ad548a809fe0fb3581d3a13c813e1
johann <johann.cavallucci@epitech.eu>
2024-05-07 14:10:57 +0200

    FIX clic -> click

--- pages/projects.vue
+++ pages/projects.vue
@@ -40,7 +40,7 @@ definePageMeta({
 <template>
   <div class="pt-5 md:pt-24 place-items-center z-9">
     <h1 class="acorn p-5 font-bold text-5xl text-text-color-primary text-center">My Projects</h1>
-    <h3 class="text-color-text font-medium text-center z-9 px-5">Here are some of my projects ( clic on the image to see the project )</h3>
+    <h3 class="text-color-text font-medium text-center z-9 px-5">Here are some of my projects ( click on the image to see the project )</h3>
     <div class="grid md:grid-cols-2 grid-cols-1 mx-auto p-2 max-w-4xl place-items-center pb-24">
       <Project
         v-for="project in projectArray"
