2aea4990db170d82eead4ee6e3291dfe1ca11b01
johann <johann.cavallucci@epitech.eu>
2024-05-21 10:42:15 +0200

    FIX - background fixed

--- components/SwitchTheme.vue
+++ components/SwitchTheme.vue
@@ -8,10 +8,6 @@ const toggleColorMode = () => {
   <SvgoLightDark
     @click="toggleColorMode"
     class="md:hidden w-7 h-7 z-20 cursor-pointer text-primary-color select-none mt-4"
-    :class="{
-      'animate-rotate180': $colorMode.preference === 'dark',
-      'animate-rotate0': $colorMode.preference === 'light',
-    }"
     :fontControlled="false"
     :alt="'Light/Dark mode'"
     :aria-label="'Light/Dark mode'"
